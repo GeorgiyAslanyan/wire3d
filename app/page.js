c93e8ce6e876026bcfbd070447b89cb392f0556b
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-08-25 12:45:49 +0500

    pixelation

--- app/page.js
+++ app/page.js
@@ -4,35 +4,43 @@ import { Wire } from "@/components/3d/Wire";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useState } from "react";
+import { EffectComposer, Pixelation } from '@react-three/postprocessing'
 
 export default function Home() {
   const [rotateSpeed, setRotateSpeed] = useState(0.4)
-  const [activeBg, setActiveBg] = useState(1)
+  const [activeBg, setActiveBg] = useState(2)
   const [model, setModel] = useState(1)
+  const [pixelation, setPixelatin] = useState(3)
 
   const activeBtn = 'p-3 rounded-lg text-white bg-blue-500 w-10 h-10 leading-[1]'
   const btn = 'p-3 rounded-lg border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white duration-300 w-10 h-10 leading-[1]'
 
 
   return (
-    <main className={activeBg == 1 ? 'bg-white' : 'bg-gradient-to-b from-zinc-800 to-zinc-600'}>
+    <main className={activeBg === 1 ? 'bg-white' : 'bg-gradient-to-b from-zinc-800 to-zinc-600'}>
       <div className="fixed top-[20px] left-[20px] backdrop-blur-xl rounded-lg border border-gray-400 bg-zinc-950 bg-opacity-70 p-4 z-[100]">
         <p className="text-white">Скорость вращения:</p>
         <div className="flex flex-wrap gap-2 my-3">
-        <button onClick={() => setRotateSpeed(0)} className={rotateSpeed == 0 ? activeBtn : btn }>0</button>
-        <button onClick={() => setRotateSpeed(0.2)} className={rotateSpeed == 0.2 ? activeBtn : btn }>1</button>
-        <button onClick={() => setRotateSpeed(0.4)} className={rotateSpeed == 0.4 ? activeBtn : btn }>2</button>
-        <button onClick={() => setRotateSpeed(0.6)} className={rotateSpeed == 0.6 ? activeBtn : btn }>3</button>
+        <button onClick={() => setRotateSpeed(0)} className={rotateSpeed === 0 ? activeBtn : btn }>0</button>
+        <button onClick={() => setRotateSpeed(0.2)} className={rotateSpeed === 0.2 ? activeBtn : btn }>1</button>
+        <button onClick={() => setRotateSpeed(0.4)} className={rotateSpeed === 0.4 ? activeBtn : btn }>2</button>
+        <button onClick={() => setRotateSpeed(0.6)} className={rotateSpeed === 0.6 ? activeBtn : btn }>3</button>
         </div>
         <p className="text-white ">Фон:</p>
         <div className="flex flex-wrap gap-2 mt-3">
-        <button onClick={() => setActiveBg(1)} className={`p-3 rounded-lg bg-white w-10 h-10 ${activeBg == 1 && "border-[2px] border-blue-500"}`}></button>
-        <button onClick={() => setActiveBg(2)} className={`p-3 rounded-lg bg-black w-10 h-10 ${activeBg == 2 && "border-[2px] border-blue-500"}`}></button>
+        <button onClick={() => setActiveBg(1)} className={`p-3 rounded-lg bg-white w-10 h-10 ${activeBg === 1 && "border-[2px] border-blue-500"}`}></button>
+        <button onClick={() => setActiveBg(2)} className={`p-3 rounded-lg bg-black w-10 h-10 ${activeBg === 2 && "border-[2px] border-blue-500"}`}></button>
         </div>
         <p className="text-white pt-3">Модель:</p>
         <div className="flex flex-wrap gap-2 mt-3">
-        <button onClick={() => setModel(1)} className={model == 1 ? activeBtn : btn }>1</button>
-        <button onClick={() => setModel(2)} className={model == 2 ? activeBtn : btn }>2</button>
+        <button onClick={() => setModel(1)} className={model === 1 ? activeBtn : btn }>1</button>
+        <button onClick={() => setModel(2)} className={model === 2 ? activeBtn : btn }>2</button>
+        </div>
+        <p className="text-white pt-3">Пикселизация:</p>
+        <div className="flex flex-wrap gap-2 mt-3">
+        <button onClick={() => setPixelatin(2)} className={pixelation === 2 ? activeBtn : btn }>1</button>
+        <button onClick={() => setPixelatin(3)} className={pixelation === 3 ? activeBtn : btn }>2</button>
+        <button onClick={() => setPixelatin(5)} className={pixelation === 5 ? activeBtn : btn }>3</button>
         </div>
       </div>
       <div className="w-full h-[100vh] z-50 relative">
@@ -59,6 +67,9 @@ export default function Home() {
               shadow-mapSize-height={1024}
             />
             <OrbitControls autoRotate autoRotateSpeed={rotateSpeed}  />
+            <EffectComposer>
+              <Pixelation granularity={pixelation}/>
+            </EffectComposer>
           </Canvas>
         </Suspense>
       </div>
